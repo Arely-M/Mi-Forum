7d4826e43866fcfc9a134c6a0f8074b3a06466d2
Arely-M <64564747+Arely-M@users.noreply.github.com>
2021-06-30 01:28:16 -0500

    frontend - backend

---
+++ Frontend/my-foro/src/components/barra.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-100">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <h4 style="background-color: white;">{{ user.nombre + " " + user.apellido }}</h4>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "barra",
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created: function () {
+    this.user = JSON.parse(localStorage.getItem("user"));
+  },
+};
+</script>
